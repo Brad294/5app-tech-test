71e8f8fd06eff045d62db4ab83471275ad24943f
Brad <bradley.h.elliott@gmail.com>
2019-03-21 20:20:14 +0000

    fixed indenting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.get("/response", (req, res) => {
   let payload = filterPayload(req.body["payload"]);
   payload = selectThumbnailUrl(payload);
-  console.log(payload)
+  console.log(payload);
   res.status(200).json({
     response: payload
   });
@@ -21,28 +21,27 @@ function filterPayload(paylaod) {
 }
 
 function selectThumbnailUrl(payload) {
-   let logoSize;
-        payload.forEach((item,payloadIdex,array)=>{
-            item.logos.forEach((logo,index,array)=>{
-                // Need to convert to some number value to be able to compare sizes in any sane way.
-              logoSize = convertToNumber(logo.size);
-              //Check to see if file size is between number range 16x6 and 64x64
-              if(logoSize> 1616 && logoSize <=6464){
-                    newArray = [logo];
-                    payload[payloadIdex].logos = newArray;
-              }else{
-                  //if not in range dont sendback anything we did'nt ask for out of range.
-                payload[payloadIdex].logos =[];
-              }
-            })
-        })
-        return payload
-    
+  let logoSize;
+  payload.forEach((item, payloadIdex, array) => {
+    item.logos.forEach((logo, index, array) => {
+      // Need to convert to some number value to be able to compare sizes in any sane way.
+      logoSize = convertToNumber(logo.size);
+      //Check to see if file size is between number range 16x6 and 64x64
+      if (logoSize > 1616 && logoSize <= 6464) {
+        newArray = [logo];
+        payload[payloadIdex].logos = newArray;
+      } else {
+        //if not in range dont sendback anything we did'nt ask for out of range.
+        payload[payloadIdex].logos = [];
+      }
+    });
+  });
+  return payload;
 }
 
-function convertToNumber(size){
-    const result = size.replace("x","")
-    return result;
+function convertToNumber(size) {
+  const result = size.replace("x", "");
+  return result;
 }
 
 module.exports = app;
